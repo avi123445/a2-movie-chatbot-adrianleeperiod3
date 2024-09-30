fe36f932bca09f77b48c99cfd04917cdab91551b
avi123445 <avikaplan1000@gmail.com>
2024-09-29 20:51:07 -0500

    Update a2.py

--- a2.py
+++ a2.py
@@ -17,38 +17,62 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
     sind = 0  # current index we are looking at in source list
     pind = 0  # current index we are looking at in pattern list
     result: List[str] = []  # to store substitutions we will return if matched
-
+    print ("avi test print 1")
     # keep checking as long as we haven't hit the end of either pattern or source while
     # pind is still a valid index OR sind is still a valid index (valid index means that
     # the index is != to the length of the list)
-    while "FILL IN CONDITION HERE":
+    # while "FILL IN CONDITION HERE":
+    while pind < len(pattern) or sind < len(source):
         # your job is to fill out the body of this loop
 
         # you should delete the following line
-        return ["Not done yet :)"]
+        # return ["Not done yet :)"]
 
         # 1) if we reached the end of the pattern but not source
-
+        print ("Avi test print 2")
+        if pind == len(pattern):
+            print ("Avi test print 3")
+            return None
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
-
+        elif pattern[pind] == '%':
+            print ("Avi test print 4", pind, sind)
+            if pind == len(pattern) - 1:
+                res = " ".join(source[sind:])
+                result.append(res)
+                return [res]
+                # pind += 1 
+                # sind = len(source)
         # 3) if we reached the end of the source but not the pattern
-
+        elif sind == len(source):
+            print ("Avi test print 5")
+            return None
         # 4) if the current thing in the pattern is an _
-
+        elif pattern[pind] == '_':
+            print ("Avi test print 6")
+            result.append(source[sind])
+            sind += 1
+            pind += 1
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
+        elif source[sind] == pattern[pind]:
+            print ("Avi test print 7")
+            pind += 1 
+            sind += 1
 
         # 6) else : this will happen if none of the other conditions are met it
         # indicates the current thing it pattern doesn't match the current thing in
         # source
-
+        else:
+            print ("Avi test print 8")
+            return None
     return result
 
-
+print("step 1")
 if __name__ == "__main__":
+#    assert match(["what", "movies", "were", "made", "in", "_"],["what", "movies", "were", "made", "in", "1974"]) == ["1974"], "test 1 failed"
     assert match(["x", "y", "z"], ["x", "y", "z"]) == [], "test 1 failed"
     assert match(["x", "z", "z"], ["x", "y", "z"]) == None, "test 2 failed"
     assert match(["x", "y"], ["x", "y", "z"]) == None, "test 3 failed"
